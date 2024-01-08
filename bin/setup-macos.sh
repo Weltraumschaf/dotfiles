fad6c1f214768f56b1f4a16ddd7a6e718178998e
Sven Strittmatter <ich@weltraumschaf.de>
2024-01-08 14:56:48 +0100

    Update Python/Ruby versions
    Signed-off-by: Sven Strittmatter <ich@weltraumschaf.de>

--- bin/setup-macos.sh
+++ bin/setup-macos.sh
@@ -4,9 +4,9 @@ set -euo pipefail
 
 # Python versions we use w/ Pyenv:
 PYTHON2_VERSION='2.7.18'
-PYTHON3_VERSION='3.12.0'
+PYTHON3_VERSION='3.12.1'
 # Ruby version we use w/ rbenv:
-RUBY_VERSION="3.2.2"
+RUBY_VERSION="3.3.0"
 
 # @see: http://wiki.bash-hackers.org/syntax/shellvars
 [ -z "${SCRIPT_DIRECTORY:-}" ] &&
